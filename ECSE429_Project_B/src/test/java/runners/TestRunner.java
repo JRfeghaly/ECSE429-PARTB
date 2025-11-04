d069bd0d24aaf093f22b1555c21e5812bfe46a60
JRfeghaly <josephfeghaly2004@gmail.com>
2025-11-04 01:53:46 -0500

    mini modification

--- ECSE429_Project_B/src/test/java/runners/TestRunner.java
+++ ECSE429_Project_B/src/test/java/runners/TestRunner.java
@@ -1,4 +1,3 @@
-// src/test/java/runners/TestRunner.java
 package runners;
 
 import org.junit.platform.suite.api.*;
@@ -6,8 +5,8 @@ import org.junit.platform.suite.api.*;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME,
-        value = "stepDefinitions")
-@ConfigurationParameter(key = io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME,
-        value = "pretty, summary, html:target/cucumber-report.html, json:target/cucumber.json")
-public class TestRunner { }
+@ConfigurationParameter(
+        key = io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-report.html"
+)
+public class TestRunner {}
--- ECSE429_Project_B/src/test/java/stepDefinitions/CommonSteps.java
+++ ECSE429_Project_B/src/test/java/stepDefinitions/CommonSteps.java
@@ -1,13 +1,30 @@
 package stepDefinitions;
 
-import io.cucumber.java.en.Given;
-import io.restassured.RestAssured;
+import io.cucumber.java.en.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.net.http.*;
+import java.net.URI;
 
 public class CommonSteps {
-    private final String BASE_URL = "http://localhost:4567";
+    
+    private static final String BASE_URL = "http://localhost:4567";
+    private static final HttpClient client = HttpClient.newHttpClient();
+    
+    // Shared response holder for all step definition classes
+    public static HttpResponse<String> lastResponse;
 
     @Given("the server is running")
-    public void theServerIsRunning() {
-        RestAssured.get(BASE_URL + "/projects").then().statusCode(200);
+    public void the_server_is_running() throws Exception {
+        HttpRequest req = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/todos"))
+                .build();
+        HttpResponse<String> res = client.send(req, HttpResponse.BodyHandlers.ofString());
+        assertTrue(res.statusCode() == 200 || res.statusCode() == 404, "Server is not running");
+    }
+
+    @Then("the response status should be {int}")
+    public void the_response_status_should_be(int code) {
+        assertNotNull(lastResponse, "No response has been recorded. Make sure a request was sent first.");
+        assertEquals(code, lastResponse.statusCode(), "Expected status " + code + " but got " + lastResponse.statusCode());
     }
 }
